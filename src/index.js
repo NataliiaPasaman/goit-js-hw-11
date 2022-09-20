b757337d72044d4dd5e9a19df5503b79a65313b5
NataliiaPasaman <natapasaman@gmail.com>
2022-09-20 23:37:47 +0300

    addEventListener on submit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import './css/styles.css';
-
 import { Notify } from 'notiflix';
+import { fetchImages } from './fetchHendler';
+
+const refs = {
+  form: document.getElementById('search-form'),
+  input: document.querySelector('[name="searchQuery"]'),
+}
+
+refs.form.addEventListener('submit', onSearchImage);
+
+function onSearchImage(event) {
+  event.preventDefault();
+
+  const searchQuery = refs.input.value;
+  console.log(refs.input.value);
+
+  fetchImages(searchQuery)
+.then(dataImages => console.log(dataImages));
+}
 
 
+
+// fetchImages()
+// .then(dataImages => console.log(dataImages));
