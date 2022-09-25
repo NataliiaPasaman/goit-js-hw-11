8f436345371b9befec59f74a6e5bb2df78c22452
NataliiaPasaman <natapasaman@gmail.com>
2022-09-25 21:02:09 +0300

    пагінація

--- src/fetchHendler.js
+++ src/fetchHendler.js
@@ -3,6 +3,7 @@ const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '30059530-99c96b166b7120acaaa07225e';
 
 // let per_page = 10;
+let page = 1;
 
 
 const searchParams = new URLSearchParams({
@@ -10,10 +11,11 @@ const searchParams = new URLSearchParams({
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    per_page: 40,
 });
 
 export function fetchImages(searchImg) {
-    return fetch(`${BASE_URL}?${searchParams}&q=${searchImg}&per_page=40&page=2`)
+    return fetch(`${BASE_URL}?${searchParams}&q=${searchImg}&page=10`)
     .then((response) => {
 
         return response.json()});
